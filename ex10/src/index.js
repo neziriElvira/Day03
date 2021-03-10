eb9bf1019a48a05dd4aaf228f4cadced25ac4ce8
Elvira Neziri <neziiri.elvira@gmail.com>
2021-03-10 16:48:57 +0100

    Worked with * operator

---
+++ ex10/src/index.js
@@ -0,0 +1,10 @@
+function main(numOne, numTwo) {
+    //Only change code below this line
+    product = numOne * numTwo;
+    //Only change code above this line
+    return product;
+}
+
+console.log(main(15, 3)); //change this line
+
+module.exports = main;
