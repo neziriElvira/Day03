5547397ccada9e63ca5b40961b287f33cd4cc222
Elvira Neziri <neziiri.elvira@gmail.com>
2021-03-10 14:05:32 +0100

    Create both types of comments

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    //Only change code below this live
+    //Hello, hello! Im a single line JavaScript comment.
+    /*Hello single line comment! Im a multi-line comment,
+    nice to meet you.
+    */
+    //Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
